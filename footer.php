d5dd3dad85ea02a87a02aff36261bd329dc1fd29
oddluck <39967334+oddluck@users.noreply.github.com>
2018-06-05 15:44:31 -0400

    Update footer.php
    Minor display changes.

--- footer.php
+++ footer.php
@@ -1,7 +1,6 @@
         </div>
         <div class="footer">
-            <p class="small">Questions? Comments? Suggestions? Bugs? Naked pics? <a href="contact.php"><?php print $admin_email?></a> or <?php print $admin_nick?>@IRC.
-<?php
+            <p class="small">Questions? Comments? Suggestions? Bugs? <a href="contact.php"><?php print $admin_email?></a> or <?php print $admin_nick?>@irc.tv-vault.me/#IdleRPub <?php
             $hits = file("hits.db");
             $fp = fopen("hits.db", "w");
             $thispage = explode("/",$_SERVER['PHP_SELF']);
@@ -13,7 +12,7 @@
                 list($page,$numhits,$date) = explode("\t",trim($line));
                 if ($page == $thispage) {
                     ++$numhits;
-                    echo "            $numhits hits since $date";
+                    echo "            $numhits hits since June 02, 2018";
                     $found = 1;
                 }
                 if ($fp) {
